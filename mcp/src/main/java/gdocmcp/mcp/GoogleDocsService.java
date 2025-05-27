f1745fc3cf98c6b9a41ed47ad94306140781c02b
arnavaggarwal75 <arnavaggarwal907@gmail.com>
2025-05-26 19:24:21 -0600

    basic react app

--- mcp/src/main/java/gdocmcp/mcp/GoogleDocsService.java
+++ mcp/src/main/java/gdocmcp/mcp/GoogleDocsService.java
@@ -13,11 +13,26 @@ import jakarta.annotation.PostConstruct;
 @Service
 public class GoogleDocsService {
 
-    private static String token = "";
+    private static String token = "ya29.a0AW4XtxjdQThJMp82KpW9udor5r_-szV1JGDTxwxe9hkejJot59_jJDbo0Wkk3SjBhhdR_WV7-DDzgsUuGOl-nfeqwpGYvsnIqzGvq4ZSv-5z9i4ZsP9pZ6n2TgVwqbzTX3gRRHoajeBzTJ0kZalnTwVBMP7KiLyc5IZG5o91aCgYKAb8SARcSFQHGX2MiV2LSdfljfTGktlx7IC9I6w0175";
+    private static String transcriptApiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6IjEifQ.eyJpc3MiOiJuYWRsZXMiLCJpYXQiOiIxNzQ4MDQ5MzYyIiwicHVycG9zZSI6ImFwaV9hdXRoZW50aWNhdGlvbiIsInN1YiI6ImI2YWYyMTE0OTBkMjRiZTg5ZDQ2NGEwYTZkZmRmMzU5In0.r512iKn-mxh3zOGqC9XUxo472FfTextzp60jQeAGeCI";
 
     @PostConstruct
     public void init() {
         System.out.println(token + " is the token");
+    }
+
+    @Tool(name = "get_transcript", description = "Retrieves the transcript of Youtube video")
+    public String getTranscript(String videoUrl) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("https://api.supadata.ai/v1/youtube/transcript?url=" + videoUrl))
+            .header("x-api-key", transcriptApiKey)
+            .timeout(Duration.ofSeconds(70))
+            .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
     }
 
     @Tool(name = "create_doc", description = "Creates a new Google Doc with a title")
