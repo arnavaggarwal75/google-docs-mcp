fcb799e6080b97e82b02d41dec3cae8741dc20d1
arnavaggarwal75 <arnavaggarwal907@gmail.com>
2025-05-26 20:20:56 -0600

    basic google auth

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div>
+      <h1>Login Page</h1>
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log("Login Success:", credentialResponse);
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+      />
+      <p>Login with Google to continue</p>
+    </div>
+  );
+};
 
 export default LoginPage;
